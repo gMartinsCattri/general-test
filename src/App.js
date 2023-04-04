b4e5529e972c3922fbd816583af46bec6c1f8273
guille-m369 <guille.ma369@gmail.com>
2023-04-04 16:26:01 -0300

    video

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [contentList, setContentList] = useState([]);
-  const [playlist, setPlaylist] = useState([]);
+  const videoRef = useRef(null);
+
   useEffect(() => {
     fetch('https://alfred.to/minideneva/mall/dev/channel/ads')
       .then(response => response.json())
       .then(data => {
-        setPlaylist(data.playlist);
         setContentList(data.playlist.map(video => video.content));
       });
   }, []);
 
-  console.log(contentList)
-  console.log(playlist)
+  useEffect(() => {
+    if (contentList.length > 0) {
+      const videoElement = videoRef.current;
+      videoElement.src = contentList[0];
+      videoElement.preload = "none";
+    }
+  }, [contentList]);
 
   useEffect(() => {
-    const videoElement = document.querySelector("#video-player");
+    const videoElement = videoRef.current;
 
-    if (contentList.length > 0) {
-      videoElement.src = contentList[0];
+    const handleVideoEnded = () => {
+      const currentIndex = contentList.indexOf(videoElement.src);
+
+      if (currentIndex < contentList.length - 1) {
+        videoElement.src = contentList[currentIndex + 1];
+        videoElement.play();
+      } else {
+        videoElement.src = contentList[0];
+        videoElement.play();
+      }
+    };
+
+    if (videoElement && contentList.length > 0) {
       videoElement.addEventListener("ended", handleVideoEnded);
     }
+
     return () => {
-      videoElement.removeEventListener("ended", handleVideoEnded);
+      if (videoElement) {
+        videoElement.removeEventListener("ended", handleVideoEnded);
+      }
     };
   }, [contentList]);
 
-  const handleVideoEnded = () => {
-    const videoElement = document.querySelector("#video-player");
-
-    const currentIndex = contentList.indexOf(videoElement.src);
-
-    if (currentIndex < contentList.length - 1) {
-      videoElement.src = contentList[currentIndex + 1];
-      videoElement.play();
-    } else {
-      videoElement.src = contentList[0];
-      videoElement.play();
-    }
-  };
-
   return (
     <div>
-      <video autoPlay id="video-player">
+      <video controls ref={videoRef}>
         
       </video>
     </div>
