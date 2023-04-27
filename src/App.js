793f448357a2f3f61dd1271a70ff54f96cecbb4e
guille-m369 <guille.ma369@gmail.com>
2023-04-27 19:34:34 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState(null);
-
+  
   useEffect(() => {
     axios
-      .get("http://local.alfred.com/deneva-service/GetNextCampaign")
+      .get("http://127.0.0.1:3000/deneva-service/GetNextCampaign")
       .then((response) => {
         const srcParts = response.data.src.split("//");
         const srcValue = srcParts[srcParts.length - 1];
-        const url = `http://local.alfred.com/deneva-service/${srcValue}`;
+        const url = `http://127.0.0.1:3000/deneva-service/${srcValue}`;
         setData(url);
         console.log(data)
       })
