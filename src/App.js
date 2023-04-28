68a3d6838386bfbb7ace03c9fb3cf93de318cf40
guille-m369 <guille.ma369@gmail.com>
2023-04-28 13:06:15 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 function VideoPlayer() {
   const [videoUrl, setVideoUrl] = useState('');
   const [videoData, setVideoData] = useState({});
+  const [campaigns, setCampaigns] = useState([]);
+  const [currentCampaignIndex, setCurrentCampaignIndex] = useState(0);
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -14,6 +16,7 @@ function VideoPlayer() {
         const url = `http://local.alfred.com/media/deneva/${srcValue}`;
         setVideoUrl(url);
         setVideoData(response.data);
+        setCampaigns([...campaigns, response.data]);
       })
       .catch(error => {
         console.error(error);
@@ -25,8 +28,38 @@ function VideoPlayer() {
     const startTime = videoRef.current.currentTime;
     const endTime = new Date().getTime() / 1000;
     const data = { id_campaing, id_resource, startTime, endTime };
-    axios.post('http://local.alfred.com/deneva-service/AuditCampaign', data);
+    axios.post('http://local.alfred.com/deneva-service/AuditCampaign', data)
+      .then(() => {
+        setCurrentCampaignIndex(currentCampaignIndex + 1);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
+
+  useEffect(() => {
+    if (currentCampaignIndex < campaigns.length) {
+      const campaign = campaigns[currentCampaignIndex];
+      const { src } = campaign;
+      const srcValue = src.split('//')[1];
+      const url = `http://local.alfred.com/media/deneva/${srcValue}`;
+      setVideoUrl(url);
+      setVideoData(campaign);
+    } else {
+      axios.get('http://local.alfred.com/deneva-service/GetNextCampaign')
+        .then(response => {
+          const { src } = response.data;
+          const srcValue = src.split('//')[1];
+          const url = `http://local.alfred.com/media/deneva/${srcValue}`;
+          setVideoUrl(url);
+          setVideoData(response.data);
+          setCampaigns([...campaigns, response.data]);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [currentCampaignIndex]);
 
   return (
     <div>
