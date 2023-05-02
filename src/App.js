80f0372826837da2987cb9b91892a035d39d86f2
guille-m369 <guille.ma369@gmail.com>
2023-05-02 14:51:20 -0300

    deneva implementada

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [videoSrc, setVideoSrc] = useState('');
-  // const [campaignData, setCampaignData] = useState({});
+  const [campaignData, setCampaignData] = useState({});
   const videoRef = useRef(null);
 
   const getNextCampaign = async () => {
     const response = await fetch('http://local.alfred.com/deneva-service/GetNextCampaign');
     const data = await response.json();
-    // setCampaignData(data);
+    setCampaignData(data);
 
     const srcValue = data.src.split('//')[1];
     const videoUrl = `http://local.alfred.com/media/deneva/${srcValue}`;
@@ -16,24 +16,28 @@ function App() {
   };
 
   const handleVideoEnded = async () => {
-    // const { id_campaing, id_resource } = campaignData;
-    // const currentTime = videoRef.current.currentTime;
-    // const timestamp = new Date().toISOString();
-
-    // const postBody = {
-    //   id_campaing,
-    //   id_resource,
-    //   startTime: timestamp,
-    //   endTime: timestamp,
-    // };
-
-    // const requestOptions = {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify(postBody),
-    // };
-
-    // await fetch('http://local.alfred.com/deneva-service/AuditCampaign', requestOptions);
+    const { id_campaign, id_resource } = campaignData;
+    const currentTime = videoRef.current.currentTime;
+    const timestamp = new Date().toISOString();
+  
+    const postBody = {
+      id_campaign,
+      id_resource,
+      startTime: timestamp,
+      endTime: timestamp,
+    };
+  
+    // Exibe as informações do postBody no console
+    console.log(postBody);
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(postBody),
+    };
+  
+    await fetch('http://local.alfred.com/deneva-service/AuditCampaign', requestOptions);
+  
     getNextCampaign();
   };
 
