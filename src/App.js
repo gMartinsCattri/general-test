24761d6da2843a7102eb7cfdcc1be9adb1be882b
guille-m369 <guille.ma369@gmail.com>
2023-05-04 12:34:08 -0300

    post en consola

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       body: JSON.stringify(postBody),
     };
     console.log('dados enviados en el body', postBody)
-    await fetch('http://127.0.0.1:3000/AuditCampaign', requestOptions);
+    await fetch('https://local.alfred.com/deneva-service/AuditCampaign', requestOptions);
     getNextCampaign();
   };
 
